27182c16ae98ae265a1c6fd35609d7b2e69ae9bb
glassole <glassole@student.21-school.ru>
2025-02-12 20:50:17 +0500

    first commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /mnt/d/pet-project/21-school_API/venv
+set -gx VIRTUAL_ENV /mnt/d/pet-project/new/21-school_API/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
